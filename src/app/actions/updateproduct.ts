580719f793a1d811e8cd1e5952a4a0706db3563f
runrvrun <runrvrun@gmail.com>
2026-02-01 21:52:43 +0700

    product update slug

--- src/app/actions/updateproduct.ts
+++ src/app/actions/updateproduct.ts
@@ -7,6 +7,7 @@ import { generateEmbedding } from "@/lib/embeddings";
 export async function updateProduct(id: BigInt, formData: FormData) {
   try {
     const name = formData.get("name") as string;
+    const slug = formData.get("slug") as string;
     const upc = formData.get("upc") as string;
     const description = formData.get("description") as string;
     const imageFile = formData.get("image") as File;
@@ -55,6 +56,7 @@ export async function updateProduct(id: BigInt, formData: FormData) {
       where: { id: Number(id) },
       data: {
         name: name.trim(),
+        slug: slug.trim(),
         upc: upc?.trim() || null,
         description: description?.trim() || null,
         // only update image if a new one was uploaded
